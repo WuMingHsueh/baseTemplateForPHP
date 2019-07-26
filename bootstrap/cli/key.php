da50b8f36410226d86e07274e15858a4e72e9a7b
Wu Ming-Hsueh <ri3567@gmail.com>
2019-07-26 13:29:39 +0800

    [beta] fix namespace

--- bootstrap/cli/key.php
+++ bootstrap/cli/key.php
@@ -4,5 +4,5 @@ require dirname(dirname(__DIR__)) . "/vendor/autoload.php";
 
 require dirname(__DIR__) . '/dependencies.php';
 
-PhpmigExample\Providers\Encryption\SodiumKey::readyKey($container);
-// PhpmigExample\Providers\Encryption\DefuseKey::readyKey($container);
+BaseTemplatePHP\Providers\Encryption\SodiumKey::readyKey($container);
+// BaseTemplatePHP\Providers\Encryption\DefuseKey::readyKey($container);
